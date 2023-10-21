48009f05fc16a721752cb57173821eb1bd02b018
Saul Sayers <saulsayers@gmail.com>
2023-10-21 15:05:34 +0700

    initial commit

---
+++ connections.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+
+    const makeNewConnection = (uri) => {
+     const db = mongoose.createConnection(uri, {
+      useNewUrlParser: true
+     });
+
+     db.on("error", function (error) {
+      console.log(`MongoDB :: connection ${this.name} ${JSON.stringify(error)}`);
+      db.close().catch(() =>
+       console.log(`MongoDB :: failed to close connection ${this.name}`)
+      );
+     });
+
+     db.on("connected", function () {
+      console.log(`MongoDB :: connected`)
+     });
+
+     db.on("disconnected", function () {
+      console.log(`MongoDB :: disconnected ${this.name}`);
+     });
+     return db;
+    };
+
+    const dbConnection = 
+     makeNewConnection('mongodb+srv://saulsayers:bUHzDZNL3WHbWpAj@cluster0.1ju3xpp.mongodb.net/?retryWrites=true&w=majority');
+
+    module.exports = dbConnection;
